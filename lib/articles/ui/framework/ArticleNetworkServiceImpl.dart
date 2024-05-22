89ec83fd81c910291d0b0121586b97b3c6518691
obidano <omotetedan2@gmail.com>
2024-05-22 13:56:32 +0100

    article  network  service

---
+++ lib/articles/ui/framework/ArticleNetworkServiceImpl.dart
@@ -0,0 +1,32 @@
+ import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_laravel/articles/business/model/categorie.dart';
+import 'package:flutter_laravel/articles/business/service/articlesNetworkservice.dart';
+import "package:http/http.dart" as http;
+
+class ArticleNetworkServiceImpl implements ArticlesNetworkService{
+  var baseURL=dotenv.env['BASE_URL'];
+
+
+  @override
+  Future<List<Categorie>> getListCategories(String token) async {
+    var res= await http.get(Uri.parse("$baseURL/api/categories"),
+        headers: {"Authorization": "Bearer $token"});
+    var reponseList=json.decode(res.body) as List;
+    print("responseMap $reponseList");
+    var responseFinal= reponseList.map((e)=> Categorie.fromJson(e) ).toList();
+    return responseFinal;
+  }
+}
+
+void main() async {
+ await dotenv.load(fileName: ".env");
+  var token="1|KE0ezAjUzFrRbvBVHvIl9VSshhiHPVtqqJKwHQhO738b167d";
+  var serviceImpl=ArticleNetworkServiceImpl();
+  var res= await serviceImpl.getListCategories(token);
+  res.forEach((v){
+    print(v.name);
+  });
+}
